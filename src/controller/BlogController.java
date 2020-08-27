a4be6cbef22dcf07a672e4793b007165f0da92c0
Chaitra6 <48185350+Chaitra6@users.noreply.github.com>
2020-08-27 18:20:34 +0530

    FSJ0820A003

--- src/controller/BlogController.java
+++ src/controller/BlogController.java
@@ -10,41 +10,70 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import model.Blog;
+import model.User;
 
-
-
-@WebServlet(urlPatterns= {"/blog"})
+@WebServlet(urlPatterns = { "/blog" })
 public class BlogController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
 
-    public BlogController() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/views/blogView.jsp");
-		rd.forward(request, response);
-		
+	public BlogController() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
 
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		RequestDispatcher rd = this.getServletContext().getRequestDispatcher("/WEB-INF/views/blogView.jsp");
+		rd.forward(request, response);
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-/*		String blogDetails = request.getParameter("selectedAnswers");
-		
-		
-		
+	}
 
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String blogDetails = request.getParameter("selectedAnswers");	
 		
-		if(blog!=null) {
+		String[] str = blogDetails.split(",");
+		String name =str[0];
+		String description = str[1];
+		LocalDate date = LocalDate.now();
+		
+		Blog blog = new Blog(name, description, date);
+		System.out.println(name);
+		System.out.println(description);
+		System.out.println(date);
+
+		blog.setTitle(name);
+		blog.setDescription(description);
+		blog.setPostedOn(date);
+		
+		if (blog != null) {
 			request.setAttribute("blog", blog);
-			request.setAttribute("user",user);
-			RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/views/blogView.jsp");
+			User user = null;
+			request.setAttribute("user", user);
+			RequestDispatcher rd = this.getServletContext().getRequestDispatcher("/WEB-INF/views/blogView.jsp");
 			rd.forward(request, response);
 		}
-	*/	
+
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
